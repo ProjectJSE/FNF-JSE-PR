aec98b5b4668c5baecf95e1140e09fe724b10feb
Altertoriel <op58274@gmail.com>
2024-08-02 04:31:29 +0900

    ok so no callbackhandler screw it

--- source/Main.hx
+++ source/Main.hx
@@ -130,7 +130,6 @@ class Main extends Sprite {
 		Mods.pushGlobalMods();
 		#end
 
-		#if LUA_ALLOWED Lua.set_callbacks_function(cpp.Callable.fromStaticFunction(psychlua.CallbackHandler.call)); #end
 		ClientPrefs.loadDefaultKeys();
 		addChild(new FlxGame(game.width, game.height, game.initialState, #if (flixel < "5.0.0") game.zoom, #end game.framerate, game.framerate, game.skipSplash, game.startFullscreen));
 
